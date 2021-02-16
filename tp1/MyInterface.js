f75b0a8b5cab8f09420d60c94b0411e00b8c1dab
Andre Julio Moreira <up201904721@fe.up.pt>
2021-02-16 17:43:21 +0000

    Implement triangle

--- tp1/MyInterface.js
+++ tp1/MyInterface.js
@@ -23,6 +23,9 @@ export class MyInterface extends CGFinterface {
         //Slider element in GUI
         this.gui.add(this.scene, 'scaleFactor', 0.1, 5).name('Scale Factor');
 
+        // Diamond checkbox
+        this.gui.add(this.scene, 'displayDiamond').name('Display Diamond');
+
         return true;
     }
 }
