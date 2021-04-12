701385d7bb1fa941f6dee06e42c9e7ae8fbc683f
Nuno Alves <up201908250@fe.up.pt>
2021-04-12 21:00:22 +0100

    add: MyCylinder class and skeleton

---
+++ project/objects/MyCylinder.js
@@ -0,0 +1,67 @@
+import { CGFobject } from "../../lib/CGF.js";
+import { MyQuad } from "../shapes/MyQuad.js"
+import { degreeToRad } from "../utils/math/MathUtils.js";
+import { translateMatrix, rotateYMatrix, mirrorYZ, mirrorXY, rotateXMatrix, scaleMatrix } from "../utils/matrix/MatrixGenerator.js";
+import { Material } from "../utils/Material.js"
+
+/**
+ * MyCylinder
+ * @constructor
+ * @param scene - Reference to MyScene
+ */
+
+export class MyCylinder extends CGFobject {
+    constructor(scene, slices) {
+		super(scene);
+        this.scene = scene;
+        this.slices = slices;
+        this.initBuffers()
+	}
+
+    initBuffers() {
+        this.vertices = [];
+        this.indices = [];
+        this.normals = [];
+        this.texCoords = [];
+
+        let angleInc = 2*Math.PI / this.slices;
+        let angle = 0;
+        let cosAngle;
+        let current = 0;
+        let past = -1;
+
+        for(let slice = 0; slice <= this.slices; slice++) {
+            cosAngle = Math.cos(angle);
+            sinAngle = Math.sin(angle);
+
+            this.vertices.push(cosAngle, 0, sinAngle);
+            this.vertices.push(cosAngle, 1, sinAngle);
+
+            if(slice > 0) {
+                this.indices.push()
+            }
+
+            angle += angleInc;
+            past++;
+            current++;
+        }
+    }
+
+    // Sets default if tex undefined
+    safeApply(tex) {
+        if (!tex) {
+            if (this.scene.defaultMaterial) this.scene.defaultMaterial.apply();
+        } else {
+            tex.getMaterial().apply();
+        }
+
+        if (!this.scene.linearRender)
+            this.scene.gl.texParameteri(this.scene.gl.TEXTURE_2D, this.scene.gl.TEXTURE_MAG_FILTER, this.scene.gl.NEAREST);
+        else
+            this.scene.gl.texParameteri(this.scene.gl.TEXTURE_2D, this.scene.gl.TEXTURE_MAG_FILTER, this.scene.gl.LINEAR);
+    }
+
+    display() {
+
+    }
+}
